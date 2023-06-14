276a07733a9b6f70160f30bb393b39bf80ba889d
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-06-14 14:12:51 +0700

    Call GitHub api

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,35 +1,22 @@
+import axios from "axios";
 import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
   state = {
-    usersData: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        url: "https://api.github.com/users/mojombo",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        node_id: "MDQ6VXNlcjI=",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        url: "https://api.github.com/users/pjhyett",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
+    usersData: [],
   };
+
+  componentDidMount() {
+    console.log("Users component mounted ...");
+    axios.get("https://api.github.com/users").then((response) => {
+      console.log(response.data);
+      this.setState({ usersData: response.data });
+    });
+  }
   render() {
     return (
-      <div>
+      <div className={userStyle}>
         {this.state.usersData.map((user) => (
           <UserItem user={user} />
         ))}
@@ -37,5 +24,11 @@ export class Users extends Component {
     );
   }
 }
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
 
 export default Users;
